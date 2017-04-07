3e400f3ed19f8539cb50abbf9eb14133ce4be705
Manoj <napestershine@gmail.com>
2017-04-07 08:50:48 +0530

    git init

---
+++ database/migrations/2017_04_05_160908_create_videos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('description');
+            $table->boolean('published');
+            $table->string('url');
+            $table->string('thumbnail');
+            $table->boolean('allow_comments');
+            $table->integer('views');
+            $table->integer('channel_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::resource(
+    'videos', 'VideoController',
+    [ 'except' => ['create', 'edit'] ]
+);
+
+Route::resource(
+    'channels', 'ChannelController',
+    [ 'except' => ['create', 'edit'] ]
+);
+
+Route::resource(
+    'comments', 'CommentController',
+    [ 'except' => ['create', 'edit'] ]
+);
+
+// Get current user
+Route::get('/me', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
